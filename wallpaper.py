38a99de3ba18e4b37f79f4863d15c41f766245ad
Apple <apple@Apples-MacBook-Pro.local>
2019-09-01 22:30:25 -0400

    added threads

--- wallpaper.py
+++ wallpaper.py
@@ -1,24 +1,41 @@
 import requests as r
+import threading as th
 import json  
 
-response = r.get('https://collectionapi.metmuseum.org/public/collection/v1/objects')
-data = response.json()['objectIDs']
-
+data = r.get('https://collectionapi.metmuseum.org/public/collection/v1/objects').json()['objectIDs']
+dataCount = len(data)
+threadCount = 4
+split = int(dataCount/threadCount)
 paintingList = []
+startIdx = 1
 
-f = open('list.txt', 'w')
+def getLinks(startIndex, split):
+    for id in range(startIndex, startIndex + split):
+    	response = r.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + str(data[id]))
+    	link = response.json()['primaryImage']
+    	importance = response.json()['isHighlight']
+    	print(id)
+    	if link and importance:
+            paintingList.add(id)
+    	else: 
+       	    print('invalid')
 
-for id in range(len(data)):
-    response = r.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + str(data[id]))
-    link = response.json()['primaryImage']
-    importance = response.json()['isHighlight']
-    print(id)
-    if (link) and (importance == True):
-        paintingList.append(id)
-        f.write(str(data[id]) + '\n')
-    else: 
-        print('invalid')
+def main():
+    global startIdx, split
+    threads = list()
+    for i in range(threadCount): 
+        t = th.Thread(target=getLinks, args=(startIdx, split))
+        t.start()
+        threads.append(t)
+        startIdx = startIdx + split
 
-f.close()
+    for i, thread in enumerate(threads):
+        thread.join() 
 
-#print(data['objectIDs'])
+    print("its over")
+
+
+if __name__ == "__main__":
+    main() 
+    
+
