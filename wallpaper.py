6b1e2432fd4b0dca87d579e69487712e5e34f8bc
Apple <apple@Apples-MacBook-Pro.local>
2019-09-03 21:49:13 -0400

    MORE THREADS

--- wallpaper.py
+++ wallpaper.py
@@ -2,23 +2,50 @@ import requests as r
 import threading as th
 import json  
 
-data = r.get('https://collectionapi.metmuseum.org/public/collection/v1/objects').json()['objectIDs']
-dataCount = len(data)
-threadCount = 4
+apiLink = 'https://collectionapi.metmuseum.org/public/collection/v1/'
+objectIDs = r.get(apiLink + 'objects').json()['objectIDs']
+dataCount = len(objectIDs)
+threadCount = 250
 split = int(dataCount/threadCount)
-paintingList = []
+paintingList = {}
 startIdx = 1
+c = 1
+lock = th.Lock()
+f = open('list.txt', 'w')
 
 def getLinks(startIndex, split):
-    for id in range(startIndex, startIndex + split):
-    	response = r.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + str(data[id]))
-    	link = response.json()['primaryImage']
-    	importance = response.json()['isHighlight']
-    	print(id)
-    	if link and importance:
-            paintingList.add(id)
-    	else: 
-       	    print('invalid')
+    for idx in range(startIndex, startIndex + split):
+        try: 
+            response = r.get(apiLink + 'objects/' + str(objectIDs[idx]))
+            link = response.json()['primaryImage']
+            importance = response.json()['isHighlight']
+            if link and importance:
+                print('Added {0} to list'.format(objectIDs[idx]))
+                paintingList[objectIDs[idx]] = {
+                    'id' :  objectIDs[idx],
+                    'link': link
+                }
+                writeToFile(json.dumps(paintingList[objectIDs[idx]]))
+
+        except: 
+            #print('Unable to currently access object {0}'.format(idx))
+            print()
+
+        finally: 
+            #incrementCounter()
+            print('{0} % \complete'.format(int(c/dataCount)*100))
+               
+def incrementCounter():
+    global c
+    lock.acquire()
+    c = c + 1
+    lock.release()
+
+def writeToFile(str):
+    global f 
+    lock.acquire()
+    f.write(str)
+    lock.release
 
 def main():
     global startIdx, split
@@ -30,12 +57,14 @@ def main():
         startIdx = startIdx + split
 
     for i, thread in enumerate(threads):
-        thread.join() 
+        thread.join()
+    
+    f.write(json.dumps(paintingList))
+    f.close() 
 
-    print("its over")
-
+def debug():
+    f.write(json.dumps(paintingList))
 
 if __name__ == "__main__":
     main() 
-    
-
+    #debug()
